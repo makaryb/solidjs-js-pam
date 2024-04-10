d2ef905cf0c685eafbcc0a5c89bf20657df2d0ad
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-04-10 22:54:13 +0300

    Actualize vite config and package json

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
 // import { readFileSync } from 'node:fs';
-// import { dirname, resolve } from 'node:path';
-// import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/solidjs-template/',
+  base: '/solidjs-js-template/',
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
     solidPlugin(),
-    tsconfigPaths(),
   ],
   // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
   // you have key and certificate. You retrieve your certificate and key using mkcert.
@@ -32,5 +31,10 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-  publicDir: './public'
+  resolve: {
+    alias: {
+      '~': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
+    },
+  },
+  publicDir: './public',
 });
