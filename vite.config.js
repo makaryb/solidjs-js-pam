907286345809100a99e6d5455eca2a731ba0699f
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-25 22:53:48 +0300

    Actualize the template

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,33 @@
-// import { readFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  base: '/solidjs-js-template/',
+  base: '/solidjs-js-template',
   plugins: [
-    /*
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
+    // Uncomment the following line to enable solid-devtools.
+    // For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     // devtools(),
     solidPlugin(),
+    // Allows using self-signed certificates to run the dev server using HTTPS.
+    // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
+    // basicSsl(),
   ],
-  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate. You retrieve your certificate and key using mkcert.
-  // Learn more:
-  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
-  //
-  // server: {
-  //   port: 443,
-  //   https: {
-  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal.pem')),
-  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal-key.pem')),
-  //   },
-  //   host: 'tma.internal',
-  // },
   build: {
     target: 'esnext',
+  },
+  publicDir: './public',
+  server: {
+    // Uncomment this line if you want to expose your dev server and access it from the devices
+    // in the same network.
+    // host: true,
   },
   resolve: {
     alias: {
       '@': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
     }
   },
-  publicDir: './public',
 });
